7cdcc5d1657de38d5c233698c540d70de54af815
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-11-25 14:36:44 +0100

    possibly final build script

--- poc_gauze_lib/docker_lib_build_script.sh
+++ poc_gauze_lib/docker_lib_build_script.sh
@@ -5,3 +5,9 @@ docker build --build-arg NUM_WORKERS=$(nproc) --target build-stage -t pmacct-bui
 TAG='_libparse_build'
 
 docker build -t libparse:$TAG -f $PGAUZE_ROOT_LOCATION/poc_gauze_lib/Dockerfile $PGAUZE_ROOT_LOCATION || exit $?
+
+CONTAINER_ID=$(docker create libparse:$TAG)
+
+docker cp $CONTAINER_ID:/usr/pmacct-gauze/poc_gauze_lib/src/.libs/libparse.so libparse.so
+
+$PMACCT_ROOT_LOCATION/test-framework/tools/pmacct_build/build_docker_images.sh -p libparse.so
